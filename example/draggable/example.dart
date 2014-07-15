60d7f9c7c90f1c6e2fc7c7279a763d059d961dd2
marcojakob <majakob@gmx.ch>
2014-07-15 23:19:15 +0200

    Example updates

--- example/draggable/example.dart
+++ example/draggable/example.dart
@@ -1,16 +1,12 @@
 library dnd_example;
 
 import 'dart:html';
-import 'package:intl/intl.dart';
-import 'package:logging/logging.dart';
 import 'package:dnd/dnd.dart';
-
-final _log = new Logger('dnd_example');
 
 Element dragAlert;
 
 main() {
-  initLogging();
+//  initLogging();
   
   dragAlert = querySelector('.alert span');
   
@@ -39,21 +35,9 @@ main() {
 
 showDragAlert(DraggableEvent event) {
   Point dist = event.position - event.startPosition;
-  dragAlert.text =  'You are dragging: element=${event.dragElement}, distance=(${dist.x},${dist.y})';
+  dragAlert.text =  'You are dragging: element=${event.draggableElement}, distance=(${dist.x},${dist.y})';
 }
 
 showDragEndAlert(DraggableEvent event) {
   dragAlert.text =  'You stopped dragging. Why would you do that? :-)';
-}
-
-initLogging() {
-  DateFormat dateFormat = new DateFormat('yyyy.mm.dd HH:mm:ss.SSS');
-  
-  // Print output to console.
-  Logger.root.onRecord.listen((LogRecord r) {
-    print('${dateFormat.format(r.time)}\t${r.loggerName}\t[${r.level.name}]:\t${r.message}');
-  });
-  
-  // Root logger level.
-  Logger.root.level = Level.FINEST;
 }
