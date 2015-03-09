7184b5cfba793fa923943fbd7fa77eee713ff73b
marcojakob <majakob@gmx.ch>
2015-03-09 13:21:26 +0100

    Testing simpler drone.io build file

--- tool/drone_io.sh
+++ tool/drone_io.sh
@@ -13,43 +13,15 @@ pub install
 pub build ${build_folder}
 
 # -----------------------------------
-# Create tmp branch with just the built files.
+# Configure git in build subfolder folder
 # -----------------------------------
-git checkout --orphan tmp
-# Remove everything except 'build'.
-find . -maxdepth 1 ! -name 'build' ! -name '.*' | xargs rm -rf
-# Move build subfolder to root.
-mv build/${build_folder}/* .
-rm -rf build
-rm .gitignore
-git add -A
-git commit -m 'initial commit from drone'
-git fetch
+
+cd build/${build_folder}
+git init
+git add .
 
 # -----------------------------------
-# Replace files on gh-pages branch with files on tmp branch.
+# Deploy to github pages.
 # -----------------------------------
-# Test if gh-pages exists.
-if git show-ref --verify --quiet refs/remotes/origin/gh-pages
-then
-  git checkout gh-pages
-  rm -rf *
-  git checkout tmp *
-  git add -A
-  # Test if we have something to commit.
-  if ! git diff-index --quiet --cached HEAD
-  then
-    git commit -m 'auto commit from drone'
-  else
-    echo There were no changes, we have nothing to commit!
-  fi
-else
-  echo No gh-pages branch, initializing gh-pages!
-  git checkout -b gh-pages
-fi
-
-# -----------------------------------
-# Push gh-pages branch.
-# -----------------------------------
-git remote set-url origin ${github_repo}
-git push origin gh-pages
+git commit -m 'deploy commit from drone'
+git push -f ${github_repo} master:gh-pages
